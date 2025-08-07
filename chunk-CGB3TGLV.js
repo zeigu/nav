5c0eb3eef953cfb2b2cf5a774e5d77b9cfabee71
zeigu <54258810+zeigu@users.noreply.github.com>
2025-08-07 11:41:21 +0000

    Deploying to gh-pages from @ zeigu/nav@823a8f7a2059b3f613e3b7cd32125bfd38318eea 🚀

---
+++ chunk-CGB3TGLV.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-5P2COS6T.js";import{pb as r,yb as e}from"./chunk-SLNPC7VB.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
